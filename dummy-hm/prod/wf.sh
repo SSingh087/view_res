9b988a00ed31c9634b0b6a6156e6706dfa9a5caa
SSingh087 <shashwat98singh@gmail.com>
2022-05-13 09:00:10 +0000

    res

---
+++ dummy-hm/prod/wf.sh
@@ -0,0 +1,8 @@
+/work/shashwat.singh/3gsky/dummy-hm/config/pycbc_make_inference \
+--config-files /work/shashwat.singh/3gsky/dummy-hm/config/workflow.ini \
+--inference-config /work/shashwat.singh/3gsky/dummy-hm/config/prior.ini \
+--injection-file /work/shashwat.singh/3gsky/dummy-hm/config/injection.hdf \
+--workflow-name gw \
+--time 300 \
+--instruments E1 E2 E3 \
+--output-dir output \
